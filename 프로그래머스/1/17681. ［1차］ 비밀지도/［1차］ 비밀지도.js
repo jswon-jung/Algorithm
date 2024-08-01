c2c746311ee673f0db174096d244bdf4a21411d9
Seungwon Jung <124873708+jswon-jung@users.noreply.github.com>
2024-08-01 21:51:51 +0900

    Update ［1차］ 비밀지도.js: 풀이 추가

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,7 +1,5 @@
+// 풀이 1.
 function solution(n, arr1, arr2) {
-  const result = [];
-  const row = [];
-
   for (let i = 0; i < n; i++) {
     arr1[i] = arr1[i].toString(2).padStart(n, "0").split("");
     arr2[i] = arr2[i].toString(2).padStart(n, "0").split("");
@@ -19,4 +17,16 @@ function solution(n, arr1, arr2) {
     }
   }
   return arr1.map((el) => el.join(""));
+}
+
+// 풀이 2.
+function solution(n, arr1, arr2) {
+  return arr1.map((map1, i) => {
+    map1 = map1.toString(2).padStart(n, "0");
+    const map2 = arr2[i].toString(2).padStart(n, "0");
+
+    return map1.split("").reduce((acc, cur, l) => {
+      return acc + (cur === "0" && map2[l] === "0" ? " " : "#");
+    }, "");
+  });
 }
