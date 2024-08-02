b3c0512cef53201773bc53c6b9bc7e3c5553573d
Seungwon Jung <124873708+jswon-jung@users.noreply.github.com>
2024-08-02 13:01:37 +0900

    Update x만큼 간격이 있는 n개의 숫자.js: 풀이 추가

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -1,3 +1,13 @@
+// 풀이 1.
 function solution(x, n) {
   return new Array(n).fill(x).map((el, i) => el*(i+1))
+}
+
+// 풀이 2.
+function solution(x, n) {
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    result.push(x * i);
+  }
+  return result;
 }
