c9123bf6c971e69cf52eef6aa2aea94248527d7d
Seungwon Jung <124873708+jswon-jung@users.noreply.github.com>
2024-08-02 14:54:39 +0900

    Update 제일 작은 수 제거하기.js: 풀이 추가

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,7 +1,16 @@
+// 풀이 1.
 function solution(arr) {
   if (arr.length === 1) return [-1];
   else {
     arr.splice(arr.indexOf(Math.min(...arr)), 1);
     return arr;
   }
+}
+
+// 풀이 2.
+function solution(arr) {
+  const result = arr.filter((el) => {
+    return el !== Math.min(...arr);
+  });
+  return arr.length !== 1 ? result : [-1];
 }
