b10ccef330f176970f5d26c24a44949ad19ed7f4
Seungwon Jung <124873708+jswon-jung@users.noreply.github.com>
2024-08-03 23:45:16 +0900

    Update 덧칠하기.js: 풀이 추가

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,3 +1,4 @@
+// 풀이 1.
 function solution(n, m, section) {
   let result = 0;
   const wall = new Array(n).fill(1);
@@ -11,5 +12,18 @@ function solution(n, m, section) {
       result++;
     }
   });
+  return result;
+}
+
+// 풀이 2.
+function solution(n, m, section) {
+  let result = 0,
+    flag = 0;
+  for (wall of section) {
+    if (wall > flag) {
+      result++;
+      flag = wall + (m - 1);
+    }
+  }
   return result;
 }
