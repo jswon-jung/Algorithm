4de855cbbe2328a898523c75aaba60b25ca4e834
Seungwon Jung <124873708+jswon-jung@users.noreply.github.com>
2024-08-12 17:37:00 +0900

    Update 둘만의 암호.js: 알고리즘 풀이 수정

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -5,14 +5,13 @@ function solution(s, skip, index) {
       let letter = el.charCodeAt();
       for (let i = 0; i < index; i++) {
         letter++;
-        if (letter > 122) letter = 97;
+        if (letter > 122) letter -= 26;
         while (skip.includes(String.fromCharCode(letter))) {
           letter++;
-          if (letter > 122) letter = 97;
+          if (letter > 122) letter -= 26;
         }
       }
       return String.fromCharCode(letter);
     })
     .join("");
 }
-
