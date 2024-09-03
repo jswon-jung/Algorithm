fc44ae15fad5ba6dbec95ea4ce0b7bd670b2d0e1
Seungwon Jung <124873708+jswon-jung@users.noreply.github.com>
2024-09-03 17:51:09 +0900

    Update 숫자의 표현.js: 풀이 추가

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,7 +1,29 @@
+// 풀이 1
 function solution(n) {
   let result = 0;
   for (let i = 0; i <= n; i++) {
     if (n % i === 0 && i % 2 === 1) result++;
+  }
+  return result;
+}
+
+// 풀이 2
+function solution(n) {
+  let result = 0;
+  for(let i=1; i<=n; i++){
+    let sum = i;
+    if(sum === n){
+      result++;
+      continue;
+    }
+    for(let j=i+1; j<=n; j++){
+      sum += j;
+      if(sum === n){
+        result++;
+        break;
+      }
+      if(sum > n) break;
+    }
   }
   return result;
 }
